86e5cd8a55816a60a090d7f9c82ffba751263859
Flavio <flavio.petruzzellis@gmail.com>
2023-09-13 18:57:17 +0200

    Fix

--- tasks/simple/itersolv_listops_mixin.py
+++ tasks/simple/itersolv_listops_mixin.py
@@ -4,32 +4,32 @@ from itersolv_data.wrapper import GeneratorWrapper
 
 class IterSolvListopsMixin:
 
-	def create_dataset(self):
+    def create_dataset(self):
         self.batch_dim = 1
-		generator = ListOpsGenerator('cuda', specials_in_x=True)
-		train_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "max_depth": self.helper.args.itersolv_listops.iid_nesting,
-		    "max_args": self.helper.args.itersolv_listops.iid_num_operands,
-		    "split": 'train',
+        generator = ListOpsGenerator('cuda', specials_in_x=True)
+        train_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "max_depth": self.helper.args.itersolv_listops.iid_nesting,
+            "max_args": self.helper.args.itersolv_listops.iid_num_operands,
+            "split": 'train',
             "s2e_baseline": True,
-		}
-		self.train_set = GeneratorWrapper(generator, train_kwargs)
+        }
+        self.train_set = GeneratorWrapper(generator, train_kwargs)
 
-		valid_iid_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "max_depth": self.helper.args.itersolv_listops.iid_nesting,
-		    "max_args": self.helper.args.itersolv_listops.iid_num_operands,
-		    "split": 'valid',
+        valid_iid_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "max_depth": self.helper.args.itersolv_listops.iid_nesting,
+            "max_args": self.helper.args.itersolv_listops.iid_num_operands,
+            "split": 'valid',
             "s2e_baseline": True,
-		}
-		self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
+        }
+        self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
 
-		valid_ood_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "max_depth": self.helper.args.itersolv_listops.ood_nesting,
-		    "max_args": self.helper.args.itersolv_listops.ood_num_operands,
-		    "split": 'valid',
+        valid_ood_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "max_depth": self.helper.args.itersolv_listops.ood_nesting,
+            "max_args": self.helper.args.itersolv_listops.ood_num_operands,
+            "split": 'valid',
             "s2e_baseline": True,
-		}
-		self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
+        }
+        self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
