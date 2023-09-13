993c282d7c3426865bd9ede143fac13f89d284b5
Flavio <flavio.petruzzellis@gmail.com>
2023-09-13 18:53:41 +0200

    Add listops task

--- tasks/simple/itersolv_listops_mixin.py
+++ tasks/simple/itersolv_listops_mixin.py
@@ -2,29 +2,34 @@ from itersolv_data.listops import ListOpsGenerator
 from itersolv_data.wrapper import GeneratorWrapper
 
 
-class IterSolvListOpsMixin:
+class IterSolvListopsMixin:
 
 	def create_dataset(self):
+        self.batch_dim = 1
 		generator = ListOpsGenerator('cuda', specials_in_x=True)
 		train_kwargs = {
-		    "batch_size": 4,
-		    "nesting": 2,
-		    "num_operands": 2,
+		    "batch_size": self.helper.args.batch_size,
+		    "max_depth": self.helper.args.itersolv_listops.iid_nesting,
+		    "max_args": self.helper.args.itersolv_listops.iid_num_operands,
+		    "split": 'train',
+            "s2e_baseline": True,
 		}
-		self.train_set = GeneratorWrapper(gen, kwargs)
+		self.train_set = GeneratorWrapper(generator, train_kwargs)
 
 		valid_iid_kwargs = {
-		    "batch_size": 4,
-		    "nesting": 2,
-		    "num_operands": 2,
+		    "batch_size": self.helper.args.batch_size,
+		    "max_depth": self.helper.args.itersolv_listops.iid_nesting,
+		    "max_args": self.helper.args.itersolv_listops.iid_num_operands,
 		    "split": 'valid',
+            "s2e_baseline": True,
 		}
-		self.valid_sets.iid = GeneratorWrapper(gen, kwargs)
+		self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
 
 		valid_ood_kwargs = {
-		    "batch_size": 4,
-		    "nesting": 4,
-		    "num_operands": 4,
+		    "batch_size": self.helper.args.batch_size,
+		    "max_depth": self.helper.args.itersolv_listops.ood_nesting,
+		    "max_args": self.helper.args.itersolv_listops.ood_num_operands,
 		    "split": 'valid',
+            "s2e_baseline": True,
 		}
-		self.valid_sets.ood = GeneratorWrapper(gen, kwargs)
+		self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
---
+++ tasks/simple/itersolv_listops_trafo.py
@@ -0,0 +1,18 @@
+from .simple_task import SimpleTask
+from .transformer_classifier_mixin import TransformerClassifierMixin
+from .itersolv_arithmetic_mixin import IterSolvListopsMixin
+from .. import task, args
+import framework
+
+
+@args
+def a(parser: framework.helpers.ArgumentParser):
+    parser.add_argument("-itersolv_listops.iid_nesting", default=2)
+    parser.add_argument("-itersolv_listops.iid_num_operands", default=2)
+    parser.add_argument("-itersolv_listops.ood_nesting", default=4)
+    parser.add_argument("-itersolv_listops.ood_num_operands", default=4)
+
+
+@task()
+class ItersolvArithmeticTrafo(IterSolvListopsMixin, TransformerClassifierMixin, SimpleTask):
+    pass
