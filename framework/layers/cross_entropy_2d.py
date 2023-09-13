0fd5b39387f410d9657acc5374db6cabea27f69d
Flavio <flavio.petruzzellis@gmail.com>
2023-09-13 11:03:10 +0200

    Fix 2d CE loss

--- framework/layers/cross_entropy_2d.py
+++ framework/layers/cross_entropy_2d.py
@@ -3,13 +3,11 @@ import torch
 
 def cross_entropy_2d(outputs, target, outputs_lengths):
 	cross_entropy_no_reduction = torch.nn.CrossEntropyLoss(reduction="none")
-	if not isinstance(outputs, list):
-		outputs = [outputs[:, pos, :] for pos in range(outputs.size(1))]
 	cumulative_loss = 0
 	pos_mask = torch.ones_like(target).cumsum(1)  # sum on cols not on rows
 	mask = pos_mask <= outputs_lengths.unsqueeze(1)
-	for char_pos, output in enumerate(outputs):
-		char_loss = cross_entropy_no_reduction(output, target[:, char_pos].squeeze())
+	for char_pos in range(target.size(1)):
+		char_loss = cross_entropy_no_reduction(outputs[:, char_pos, :], target[:, char_pos])
 		masked_char_loss = char_loss * mask[:, char_pos]
 		cumulative_loss += masked_char_loss.sum()
 	avg_loss = cumulative_loss / mask.sum()
