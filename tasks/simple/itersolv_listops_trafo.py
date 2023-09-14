6473375d9cd496f15f002661d9801af34b0f279f
Flavio <flavio.petruzzellis@gmail.com>
2023-09-14 12:49:21 +0200

    Change IID params arithm listops

--- tasks/simple/itersolv_listops_trafo.py
+++ tasks/simple/itersolv_listops_trafo.py
@@ -8,7 +8,7 @@ import framework
 @args
 def a(parser: framework.helpers.ArgumentParser):
     parser.add_argument("-itersolv_listops.iid_nesting", default=2)
-    parser.add_argument("-itersolv_listops.iid_num_operands", default=2)
+    parser.add_argument("-itersolv_listops.iid_num_operands", default=3)
     parser.add_argument("-itersolv_listops.ood_nesting", default=4)
     parser.add_argument("-itersolv_listops.ood_num_operands", default=4)
 
