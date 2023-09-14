6975b74dee0051a6ecda0ff81dda32d5841aa47a
Flavio <flavio.petruzzellis@gmail.com>
2023-09-14 12:54:59 +0200

    Fix

--- tasks/simple/itersolv_algebra_mixin.py
+++ tasks/simple/itersolv_algebra_mixin.py
@@ -4,32 +4,32 @@ from itersolv_data.wrapper import GeneratorWrapper
 
 class IterSolvAlgebraMixin:
 
-	def create_datasets(self):
+    def create_datasets(self):
         self.batch_dim = 1
-		generator = AlgebraicExpressionGenerator('cuda', specials_in_x=True)
-		train_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "nesting": self.helper.args.itersolv_arithmetics.iid_nesting,
-		    "num_operands": self.helper.args.itersolv_arithmetics.iid_num_operands,
-		    "split": 'train',
+        generator = AlgebraicExpressionGenerator('cuda', specials_in_x=True)
+        train_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "nesting": self.helper.args.itersolv_arithmetics.iid_nesting,
+            "num_operands": self.helper.args.itersolv_arithmetics.iid_num_operands,
+            "split": 'train',
             "s2e_baseline": True,
-		}
-		self.train_set = GeneratorWrapper(generator, train_kwargs)
+        }
+        self.train_set = GeneratorWrapper(generator, train_kwargs)
 
-		valid_iid_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "nesting": self.helper.args.itersolv_arithmetics.iid_nesting,
-		    "num_operands": self.helper.args.itersolv_arithmetics.iid_num_operands,
-		    "split": 'valid',
+        valid_iid_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "nesting": self.helper.args.itersolv_arithmetics.iid_nesting,
+            "num_operands": self.helper.args.itersolv_arithmetics.iid_num_operands,
+            "split": 'valid',
             "s2e_baseline": True,
-		}
-		self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
+        }
+        self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
 
-		valid_ood_kwargs = {
-		    "batch_size": self.helper.args.batch_size,
-		    "nesting": self.helper.args.itersolv_arithmetics.ood_nesting,
-		    "num_operands": self.helper.args.itersolv_arithmetics.ood_num_operands,
-		    "split": 'valid',
+        valid_ood_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "nesting": self.helper.args.itersolv_arithmetics.ood_nesting,
+            "num_operands": self.helper.args.itersolv_arithmetics.ood_num_operands,
+            "split": 'valid',
             "s2e_baseline": True,
-		}
-		self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
+        }
+        self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
