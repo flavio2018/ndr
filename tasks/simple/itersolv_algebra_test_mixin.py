1261cd2d6d9138ea5096f9b91d05a0336de12ef5
Flavio <flavio.petruzzellis@gmail.com>
2023-09-19 18:44:43 +0200

    Algebra test task

--- tasks/simple/itersolv_algebra_test_mixin.py
+++ tasks/simple/itersolv_algebra_test_mixin.py
@@ -9,32 +9,11 @@ class IterSolvAlgebraTestMixin:
         generator = AlgebraicExpressionGenerator('cuda', specials_in_x=True,
                                                  variables='xy',
                                                  coeff_variables='ab')
-        valid_ood_kwargs = {
+        train_kwargs = {
             "batch_size": self.helper.args.batch_size,
             "nesting": 2,
-            "num_operands": 2,
-            "split": 'test',
+            "num_operands": 3,
+            "split": 'train',
             "s2e_baseline": True,
-            "exact": True
         }
-        self.valid_sets.ood_2_2 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 3
-        self.valid_sets.ood_2_3 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 4
-        self.valid_sets.ood_2_4 = GeneratorWrapper(generator, valid_ood_kwargs)
-        
-        valid_ood_kwargs['nesting'] = 3
-        valid_ood_kwargs['num_operands'] = 2
-        self.valid_sets.ood_3_2 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 3
-        self.valid_sets.ood_3_3 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 4
-        self.valid_sets.ood_3_4 = GeneratorWrapper(generator, valid_ood_kwargs)
-
-        valid_ood_kwargs['nesting'] = 4
-        valid_ood_kwargs['num_operands'] = 2
-        self.valid_sets.ood_4_2 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 3
-        self.valid_sets.ood_4_3 = GeneratorWrapper(generator, valid_ood_kwargs)
-        valid_ood_kwargs['num_operands'] = 4
-        self.valid_sets.ood_4_4 = GeneratorWrapper(generator, valid_ood_kwargs)
+        self.train_set = GeneratorWrapper(generator, train_kwargs)
