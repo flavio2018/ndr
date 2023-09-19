de9a068b43c871814b5e48e44a1ffbe818671204
Flavio <flavio.petruzzellis@gmail.com>
2023-09-19 18:49:07 +0200

    Arithmetic test task

---
+++ tasks/simple/itersolv_arithmetic_test_mixin.py
@@ -0,0 +1,18 @@
+from itersolv_data.arithmetic import ArithmeticExpressionGenerator
+from itersolv_data.wrapper import GeneratorWrapper
+
+
+class IterSolvArithmeticTestMixin:
+
+	def create_datasets(self):
+		self.batch_dim = 1
+		generator = ArithmeticExpressionGenerator('cuda', specials_in_x=True)
+        
+        train_kwargs = {
+            "batch_size": self.helper.args.batch_size,
+            "nesting": 2,
+            "num_operands": 3,
+            "split": 'train',
+            "s2e_baseline": True,
+        }
+        self.train_set = GeneratorWrapper(generator, train_kwargs)
---
+++ tasks/simple/itersolv_arithmetic_trafo_test.py
@@ -0,0 +1,9 @@
+from .simple_task import SimpleTask
+from .transformer_classifier_mixin import TransformerClassifierMixin
+from .itersolv_arithmetic_test_mixin import IterSolvArithmeticTestMixin
+from .. import task
+
+
+@task
+class ItersolvArithmeticTrafoTest(IterSolvArithmeticTestMixin, TransformerClassifierMixin, SimpleTask):
+	pass
