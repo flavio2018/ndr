40e530507ac4cd32deffe837c2eb6a54d9c7a764
Flavio <flavio.petruzzellis@gmail.com>
2023-10-02 19:12:38 +0200

    Fix

--- itersolv_data/test_dataset.py
+++ itersolv_data/test_dataset.py
@@ -1,7 +1,7 @@
 from .AbstractGenerator import _PAD
-from algebra import AlgebraicExpressionGenerator
-from arithmetic import ArithmeticExpressionGenerator
-from listops import ListopsGenerator
+from .algebra import AlgebraicExpressionGenerator
+from .arithmetic import ArithmeticExpressionGenerator
+from .listops import ListopsGenerator
 import torch
 import framework
 import pandas as pd
