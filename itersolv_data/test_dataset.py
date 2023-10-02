0ac29b48b3a3ade690f10797b3572bf7cd02cb9b
Flavio <flavio.petruzzellis@gmail.com>
2023-10-02 19:19:27 +0200

    Fix

--- itersolv_data/test_dataset.py
+++ itersolv_data/test_dataset.py
@@ -19,7 +19,7 @@ class TestDataset(torch.utils.data.Dataset):
         df = pd.read_csv(f'dataset/itersolv/{self.task_name}/test/nesting-{self.kwargs["nesting"]}_num-operands-{self.kwargs["num_operands"]}.csv')
         inputs = df['input'].tolist()[:self.kwargs['batch_size']]
         target = df['target'].tolist()[:self.kwargs['batch_size']]
-        self.X = self.generator.str_to_batch(batch)
+        self.X = self.generator.str_to_batch(inputs)
         self.target = self.generator.str_to_batch(target, x=False)
 
     @property
