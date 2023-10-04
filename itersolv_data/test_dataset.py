c1f721cf3b3ffb74b8fceb901a080dd3c733d50d
Flavio <flavio.petruzzellis@gmail.com>
2023-10-04 16:13:31 +0200

    Sliceable test dataset

--- itersolv_data/test_dataset.py
+++ itersolv_data/test_dataset.py
@@ -15,12 +15,19 @@ class TestDataset(torch.utils.data.IterableDataset):
             self.in_vocabulary = framework.data_structures.WordVocabulary([c for c in self.generator.x_vocab.vocab.itos_])
             self.out_vocabulary = framework.data_structures.WordVocabulary([c for c in self.generator.y_vocab.vocab.itos_])
 
-    def build_input_target(self):
+    def build_input_target_slices(self):
         df = pd.read_csv(f'dataset/itersolv/{self.task_name}/test/nesting-{self.kwargs["nesting"]}_num-operands-{self.kwargs["num_operands"]}.csv')
-        inputs = df['input'].tolist()[:self.kwargs['batch_size']]
-        target = df['target'].astype(str).tolist()[:self.kwargs['batch_size']]
-        self.X = self.generator.str_to_batch(inputs)
-        self.target = self.generator.str_to_batch(target, x=False)
+        bs = bs
+        
+        num_slices = len(df) // bs
+        if (len(df) % bs != 0):
+            num_slices += 1
+        
+        for slice_idx in range(num_slices):
+            inputs_slice = df['input'].tolist()[slice_idx*bs:slice_idx*bs+bs]
+            target_slice = df['target'].astype(str).tolist()[slice_idx*bs:slice_idx*bs+bs]
+            self.X_slices.append(self.generator.str_to_batch(inputs_slice))
+            self.target_slices.append(self.generator.str_to_batch(target_slice, x=False))
 
     @property
     def task_name(self):
@@ -37,7 +44,7 @@ class TestDataset(torch.utils.data.IterableDataset):
         self.in_vocabulary = None
         self.out_vocabulary = None
         self.construct_vocab()
-        self.build_input_target()
+        self.build_input_target_slices()
 
     def __len__(self):
         return self.X.size(0)
@@ -50,13 +57,14 @@ class TestDataset(torch.utils.data.IterableDataset):
             pad_idx = self.generator.y_vocab[_PAD]
             return (batch != pad_idx).sum(-1)
 
-        token_X, token_Y = self.X.argmax(-1), self.target.argmax(-1)
-        yield {
-                "in": token_X.T,
-                "out": token_Y,
-                "in_len": _sample_len(token_X),
-                "out_len": _sample_len(token_Y),
-            }
+        for X_slice, target_slice in zip(self.X_slices, self.target_slices):
+            token_X, token_Y = X_slice.argmax(-1), target_slice.argmax(-1)
+            yield {
+                    "in": token_X.T,
+                    "out": token_Y,
+                    "in_len": _sample_len(token_X),
+                    "out_len": _sample_len(token_Y),
+                }
 
     def start_test(self) -> TextClassifierTestState:
         return TextClassifierTestState(lambda x: " ".join(self.in_vocabulary(x)),
