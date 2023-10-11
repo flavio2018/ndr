5cc61d8cf680af92ee90ea168e554e8d5da21e00
Flavio <flavio.petruzzellis@gmail.com>
2023-10-11 16:52:29 +0200

    Test use generator wrapper

--- eval_itersolv_data.py
+++ eval_itersolv_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from main import initialize
 from itersolv_data.test_dataset import TestDataset
+from itersolv_data.wrapper import GeneratorWrapper
 
 
 def main():
@@ -21,7 +22,8 @@ def main():
         for n_operands in range(2, 5):
             valid_ood_kwargs["nesting"] = nesting
             valid_ood_kwargs["num_operands"] = n_operands
-            task.valid_sets.ood = TestDataset(task.train_set.generator, valid_ood_kwargs)
+            # task.valid_sets.ood = TestDataset(task.train_set.generator, valid_ood_kwargs)
+            task.valid_sets.ood = GeneratorWrapper(task.train_set.generator, valid_ood_kwargs)
             task.create_loaders()
             test, loss = task.validate_on_name('ood')
             print(f'ood_{nesting}_{n_operands}', test.accuracy)
