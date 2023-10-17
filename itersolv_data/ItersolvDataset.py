682ae8173940289fc8afa45b6494d5d0c88725cf
Flavio <flavio.petruzzellis@gmail.com>
2023-10-17 21:08:57 +0200

    Print

--- itersolv_data/ItersolvDataset.py
+++ itersolv_data/ItersolvDataset.py
@@ -51,9 +51,8 @@ class ItersolvDataset(torch.utils.data.IterableDataset):
         while _continue():
             batch_df = self.df.sample(n=self.batch_size)
             X, Y = batch_df['X'].tolist(), batch_df['Y'].tolist()
-            # Y = [f'?{y}.' for y in Y]   # add SOS and EOS
-            print(X[:10], Y[:10])
-            batch_X, batch_Y = self.generator.str_to_batch(X), self.generator.str_to_batch(X, x=False)
+            Y = [f'?{y}.' for y in Y]   # add SOS and EOS
+            batch_X, batch_Y = self.generator.str_to_batch(X), self.generator.str_to_batch(Y, x=False)
             token_X, token_Y = batch_X.argmax(-1), batch_Y.argmax(-1)
             yield {
                 "in": token_X.T,
