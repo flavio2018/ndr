f0266dd80ccd49a11e6434fb07e5e4df3dc49b49
Flavio <flavio.petruzzellis@gmail.com>
2023-10-17 22:03:23 +0200

    Cast X to str

--- itersolv_data/ItersolvDataset.py
+++ itersolv_data/ItersolvDataset.py
@@ -50,8 +50,7 @@ class ItersolvDataset(torch.utils.data.IterableDataset):
         
         while _continue():
             batch_df = self.df.sample(n=self.batch_size)
-            X, Y = batch_df['X'].tolist(), batch_df['Y'].astype(str).tolist()
-            print(Y[:10])
+            X, Y = batch_df['X'].astype(str).tolist(), batch_df['Y'].astype(str).tolist()
             batch_X, batch_Y = self.generator.str_to_batch(X), self.generator.str_to_batch(Y, x=False)
             token_X, token_Y = batch_X.argmax(-1), batch_Y.argmax(-1)
             yield {
