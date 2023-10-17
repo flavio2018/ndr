5fa837ea6534ce15d241f85c08fbcecb68143f71
Flavio <flavio.petruzzellis@gmail.com>
2023-10-17 22:07:13 +0200

    Listops

--- tasks/simple/itersolv_listops_mixin.py
+++ tasks/simple/itersolv_listops_mixin.py
@@ -1,5 +1,5 @@
 from itersolv_data.listops import ListOpsGenerator
-from itersolv_data.wrapper import GeneratorWrapper
+from itersolv_data.ItersolvDataset import ItersolvDataset
 
 
 class IterSolvListopsMixin:
@@ -7,29 +7,10 @@ class IterSolvListopsMixin:
     def create_datasets(self):
         self.batch_dim = 1
         generator = ListOpsGenerator('cuda', specials_in_x=True, ops='ias')
-        train_kwargs = {
-            "batch_size": self.helper.args.batch_size,
-            "nesting": self.helper.args.itersolv_listops.iid_nesting,
-            "n_operands": self.helper.args.itersolv_listops.iid_num_operands,
-            "split": 'train',
-            "s2e_baseline": True,
-        }
-        self.train_set = GeneratorWrapper(generator, train_kwargs)
+        
+        self.train_set = ItersolvDataset(generator, 'listops', 'train', self.helper.args.batch_size)
 
-        valid_iid_kwargs = {
-            "batch_size": self.helper.args.batch_size,
-            "nesting": self.helper.args.itersolv_listops.iid_nesting,
-            "n_operands": self.helper.args.itersolv_listops.iid_num_operands,
-            "split": 'valid',
-            "s2e_baseline": True,
-        }
-        self.valid_sets.iid = GeneratorWrapper(generator, valid_iid_kwargs)
-
-        valid_ood_kwargs = {
-            "batch_size": self.helper.args.batch_size,
-            "nesting": self.helper.args.itersolv_listops.ood_nesting,
-            "n_operands": self.helper.args.itersolv_listops.ood_num_operands,
-            "split": 'valid',
-            "s2e_baseline": True,
-        }
-        self.valid_sets.ood = GeneratorWrapper(generator, valid_ood_kwargs)
+        self.valid_sets.iid = ItersolvDataset(generator, 'listops', 'valid_iid', self.helper.args.batch_size)
+        
+        self.valid_sets.ood = ItersolvDataset(generator, 'listops', 'valid_ood', self.helper.args.batch_size)
+        
