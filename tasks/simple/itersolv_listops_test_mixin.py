91905d4c185361ff0f2faa438c35353651c23bd8
Flavio <flavio.petruzzellis@gmail.com>
2023-10-18 18:08:20 +0200

    Use ItersolvDataset in eval

--- tasks/simple/itersolv_listops_test_mixin.py
+++ tasks/simple/itersolv_listops_test_mixin.py
@@ -8,11 +8,4 @@ class IterSolvListopsTestMixin:
         self.batch_dim = 1
         generator = ListOpsGenerator('cuda', specials_in_x=True, ops='ias')
         
-        train_kwargs = {
-            "batch_size": self.helper.args.batch_size,
-            "nesting": 2,
-            "n_operands": 3,
-            "split": 'train',
-            "s2e_baseline": True,
-        }
-        self.train_set = GeneratorWrapper(generator, train_kwargs)
+        self.train_set = ItersolvDataset(generator, 'listops', 'train', self.helper.args.batch_size)
