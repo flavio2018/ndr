9d53b6c9efe94e714378a1ff086ce4d6247aa502
Flavio <flavio.petruzzellis@gmail.com>
2024-06-27 11:17:18 +0200

    Listops depth 6

--- eval_itersolv_data.py
+++ eval_itersolv_data.py
@@ -13,8 +13,10 @@ def main():
         difficulty_splits = [[1, 2], [1, 3], [1, 4],
                              [2, 2], [2, 3], [2, 4],
                              [3, 2], [3, 3], [3, 4],
-                             [4, 2], [4, 3], [4, 4]]
-        accuracy_table = pd.DataFrame(index=[1, 2, 3, 4], columns=[2, 3, 4])
+                             [4, 2], [4, 3], [4, 4],
+                             [5, 2], [5, 3], [5, 4],
+                             [6, 2], [6, 3], [6, 4]]
+        accuracy_table = pd.DataFrame(index=[1, 2, 3, 4, 5, 6], columns=[2, 3, 4])
 
     elif task_name in ['arithmetic', 'algebra']:
         difficulty_splits = [[1, 2], [2, 2], [3, 2],
