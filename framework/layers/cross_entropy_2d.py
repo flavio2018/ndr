c529ff73493b0b3a9abee39ce4ebf7ab4ebf3a03
Flavio <flavio.petruzzellis@gmail.com>
2024-07-28 11:54:03 +0200

    Fix

--- framework/layers/cross_entropy_2d.py
+++ framework/layers/cross_entropy_2d.py
@@ -6,8 +6,8 @@ def cross_entropy_2d(outputs, target, targets_lengths):
 	pos_mask = torch.ones_like(target).cumsum(1)
 	mask = pos_mask <= targets_lengths.unsqueeze(1)
 	outputs = outputs.permute(0, 2, 1)
-	if target.shape[1] > outputs.shape[1]:
-		outputs = outputs[:, :target.shape[1], :]
+	if target.shape[1] < outputs.shape[2]:
+		outputs = outputs[:, :, :target.shape[1]]
 	batch_loss = cross_entropy_no_reduction(outputs, target)
 	masked_batch_loss = batch_loss * mask
 	cumulative_loss = masked_batch_loss.sum()
